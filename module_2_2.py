10e9c4d9cb8d3b475c3c4d25271675cfa36c932e
Forshteven <geocinds@mail.ru>
2024-08-19 05:47:15 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,8 +1,9 @@
-grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-list_of_students = sorted(students)
-print(list_of_students)
-middle = [sum(i)/len(i) for i in grades]
-print(middle)
-k = dict(zip(list_of_students, middle))
-print(k)
+first = int(input('Введите число от 1 до 10: '))
+second = int(input('Введите число от 1 до 10: '))
+third = int(input('Введите число от 1 до 10: '))
+if first == second == third:
+    print(3)
+elif first == second != third or first == third != second or first != second == third:
+    print(2)
+else:
+    print(0)
